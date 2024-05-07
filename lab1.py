6c3b828e19c7400f70d985c375ef50625fc2593b
Snarkwoz <140124176+Snarkwoz@users.noreply.github.com>
2024-05-07 11:50:11 +1000

    car

--- lab1.py
+++ lab1.py
@@ -13,6 +13,7 @@ class Car:
         self.color = color
         self.brand = brand
         self.is_engine_on = False
+        self.odometer = 0
 
     def start_engine(self):
         self.is_engine_on = True
@@ -22,19 +23,26 @@ class Car:
         self.is_engine_on = False
         print(f"The engine of the {self.color} {self.brand} is now off.")
 
+    def zoomzoom(self, distance):
+        if (self.is_engine_on):
+            self.odometer = distance
+            print(f"You do be zoomin")
+        else:
+            print("Engine not on dumbass")
+
 
 # Add another property to the Car class called "odometer".
 # This property should be initialised to 0.
 
 
-
 # Create two Car objects. One should be a red Toyota and the other a blue Ford.
 
-
+Toyota = Car("Red", "Toyota")
+Ford = Car("Blue", "Ford")
 
 # Start the engine of the red Toyota.
 
-
+Toyota.start_engine()
 
 # Create a method called "drive" that takes a distance as a parameter.
 # The car can only be driven if the engine is on.
@@ -43,7 +51,8 @@ class Car:
 
 # Attempt to drive both cars 100Km.
 
-
+Toyota.zoomzoom(100)
+Ford.zoomzoom(100)
 
 # Print the brand, odometer and colour of both cars.
 
